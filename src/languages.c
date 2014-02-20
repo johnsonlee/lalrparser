5a968b87951d2a04f0c1ba45fd1222804e5a0daf
Johnson Lee <g.johnsonlee@gmail.com>
2014-02-20 13:45:10 -0500

    Remove auto generated files

--- src/languages.c
+++ src/languages.c
@@ -1,8 +1,10 @@
 #include <lalr/language-manager.h>
 
+extern parser_t javascript_parser;
 extern parser_t json_parser;
 
 struct language languages[] = {
+    { "javascript", &javascript_parser },
     { "json", &json_parser },
     NULL
 };
